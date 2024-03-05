d05f9009f3c382f65b3c752811a30b627e9e8a26
Falcho <m.k.falcham@gmail.com>
2024-03-05 12:23:50 +0100

    updates

--- DrinksMenu.java
+++ DrinksMenu.java
@@ -10,6 +10,7 @@ class DrinksMenu{
     int age = Integer.parseInt(input);
 
     ArrayList<String> options = new ArrayList<>();
+    	options.add("Vand");
     	if(age > 18){
     		options.add("Gin&Tonic");
     		options.add("Rom&Cola");
@@ -17,26 +18,29 @@ class DrinksMenu{
     		options.add("Cola");
     		options.add("Saftevand");
     	}
+    	//options.add("afslut")
 	
-	for(String option: options){
-		System.out.println(option);
-
-	}
 	System.out.println("Hvor mange drinks vil du bestille?");
 	int numberOfDrinks = Integer.parseInt(scan.nextLine());
+
 
 	ArrayList<String> choices = new ArrayList<>();
 	while(choices.size() < numberOfDrinks){
 		System.out.println("Vaelg en drink fra listen");
+			for(String option: options){
+		System.out.println(options.indexOf(option)+1+": "+option);
+	}
 		String choice = scan.nextLine();
 		if(options.contains(choice)){
 		choices.add(choice);
 	}else{
-		System.out.println("Dette er ikke end del af vores udvalg, prøv igen.");
+		System.out.println("Dette er ikke end del af vores udvalg, proev igen.");
 	}
 	}
-	System.out.println("Du har valgt: "+choices);
-
+	System.out.println("Du har valgt: ");
+	for(String chosen:choices){
+		System.out.println(chosen);
+}
 }
 }
 	
