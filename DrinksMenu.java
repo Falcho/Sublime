0e957bb8287feb6bb797aa38c7da647ac89b56c2
Falcho <m.k.falcham@gmail.com>
2024-03-07 09:07:52 +0100

    Updates

--- DrinksMenu.java
+++ DrinksMenu.java
@@ -1,46 +1,46 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-class DrinksMenu{
-	public static void main(String[]args){
-		Scanner scan = new Scanner(System.in);
+class DrinksMenu {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<String> options = new ArrayList<>();
+        ArrayList<String> choices = new ArrayList<>();
 
-	System.out.println("Hvor gammel er du?");
-	String input = scan.nextLine();
-    int age = Integer.parseInt(input);
-
-    ArrayList<String> options = new ArrayList<>();
-    	options.add("Vand");
-    	if(age > 18){
-    		options.add("Gin&Tonic");
-    		options.add("Rom&Cola");
-    	}else{
-    		options.add("Cola");
-    		options.add("Saftevand");
-    	}
-    	//options.add("afslut")
-	
-	System.out.println("Hvor mange drinks vil du bestille?");
-	int numberOfDrinks = Integer.parseInt(scan.nextLine());
+        System.out.println("Hvor gammel er du?");
+        String input = scan.nextLine();
+        int age = Integer.parseInt(input);
 
 
-	ArrayList<String> choices = new ArrayList<>();
-	while(choices.size() < numberOfDrinks){
-		System.out.println("Vaelg en drink fra listen");
-			for(String option: options){
-		System.out.println(options.indexOf(option)+1+": "+option);
-	}
-		String choice = scan.nextLine();
-		if(options.contains(choice)){
-		choices.add(choice);
-	}else{
-		System.out.println("Dette er ikke end del af vores udvalg, proev igen.");
-	}
-	}
-	System.out.println("Du har valgt: ");
-	for(String chosen:choices){
-		System.out.println(chosen);
+        options.add("Vand");
+        if (age > 18) {
+            options.add("Gin&Tonic");
+            options.add("Rom&Cola");
+        } else {
+            options.add("Cola");
+            options.add("Saftevand");
+        }
+        //options.add("afslut")
+
+        System.out.println("Hvor mange drinks vil du bestille?");
+        int numberOfDrinks = Integer.parseInt(scan.nextLine());
+
+
+        while (choices.size() < numberOfDrinks) {
+            System.out.println("Vaelg en drink fra listen");
+            for (String option : options) {
+                System.out.println(options.indexOf(option) + 1 + ": " + option);
+            }
+            int choice = scan.nextInt();
+            if (options.contains(choice)) {
+                choices.add(choice);
+            } else {
+                System.out.println("Dette er ikke end del af vores udvalg, proev igen.");
+            }
+        }
+        System.out.println("Du har valgt: ");
+        for (String chosen : choices) {
+            System.out.println(chosen);
+        }
+    }
 }
-}
-}
-	
---
+++ StudentArray.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class StudentArray{
+
+	ArrayList<String>studentNames(String name);
+
+	public static void main(String[]args){
+
+		studentNames.add("Mikkel");
+		studentNames.add("Isabella");
+		studentNames.add("Kasper");
+		studentNames.add("Julie");
+		studentNames.add("Phillip");
+
+	}
+
+}
